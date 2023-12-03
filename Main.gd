38a5b12ec1dae202e487434b96188c8e88c3f278
Somron Touch <somrontouche@gmail.com>
2023-12-03 01:52:36 -0500

    Added Player and Monster Collision and Knockback (#1)

---
+++ Main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+const WORLD_SCENE = "res://world.tscn"
+
+# Quit window when user clicks quit
+func _on_quit_pressed():
+	get_tree().quit()
+
+# Change Scene to World when user clicks play
+func _on_play_pressed():
+	get_tree().change_scene_to_file(WORLD_SCENE)
---
+++ Player/Player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const SPEED = 50.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func _process(delta):
+	velocity.x = SPEED
+
+	move_and_slide()
+		
+func _on_body_enter(body):
+	if body.is_in_group("Slime"):
+		print("hello")
+		
+func applyKnockback():
+	var knockback_direction = Vector2.LEFT
+	var knockback_strength = 500.0
+	
+	if is_on_floor():
+		velocity = knockback_direction.normalized() * knockback_strength
+	
+	move_and_slide()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Slime"):
+		applyKnockback()
+		print("collided with Slime")
