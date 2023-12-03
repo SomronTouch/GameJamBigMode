ffa53d431cf33ada71a8bed903e1ee0541baf966
SomronTouch <somrontouche@gmail.com>
2023-12-03 01:49:48 -0500

    Added Player and Monster Collision and Knockback

---
+++ Monster/slime.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+
+const SPEED = -50.0
+var knockback = false
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	move_and_slide()
+	
+func _process(delta):
+	if knockback:
+		velocity.x = SPEED
+	
+func applyKnockback():
+	var knockback_direction = Vector2.RIGHT
+	var knockback_strength = 500.0
+	
+	if is_on_floor():
+		velocity = knockback_direction.normalized() * knockback_strength
+	knockback = true
+	move_and_slide()
+
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Player"):
+		applyKnockback()
+		print("Collision with Player")
