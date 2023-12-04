1f620881c5cd1c741696ba9ce8d28089d4fd8ddb
SomronTouch <somrontouche@gmail.com>
2023-12-04 03:47:56 -0500

    added a simple timer

---
+++ ui-elements/timer/timer.gd
@@ -0,0 +1,15 @@
+extends RichTextLabel
+var game_start_time
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	game_start_time = Time.get_ticks_msec()
+func _process(delta):
+	set_text(get_time())
+
+func get_time():
+	var current_time = Time.get_ticks_msec() - game_start_time
+	var minutes = current_time/1000/60
+	var seconds = current_time/1000%60
+	var miliseconds = current_time%1000/10
+	return str("%02d:%02d:%02d" % [minutes,seconds,miliseconds])
+
