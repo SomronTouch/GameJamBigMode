54293aaa6ae414001034021bded5e2bc6511103c
SomronTouch <somrontouche@gmail.com>
2023-12-06 11:52:13 -0500

    added health component and enemy resource and udpated git ignore

---
+++ scripts/enemy_resource.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name EnemyResource
+	
+@export var enemyName:String
+@export var enemyHealth:int
+@export var enemyDamage:int
+@export var movementSpeed: int
+@export var knockbackDistance: int
+
