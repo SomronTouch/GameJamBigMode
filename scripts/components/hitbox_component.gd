13962d425b45c50e95c848cc3e19208b8e477c07
William Raymond <william.c.raymond@protonmail.com>
2023-12-08 15:23:51 -0500

    Implement hitbox component (#17)

---
+++ scripts/components/hitbox_component.gd
@@ -0,0 +1,8 @@
+class_name HitboxComponent
+extends Area2D
+
+# export variables
+@export var health_component: HealthComponent
+
+# onready variables
+@onready var hitbox_component: Area2D = $HitboxComponent
